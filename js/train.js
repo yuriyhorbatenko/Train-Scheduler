d00a1bc4f40964119d0112ea3082831ca9da6388
Yuriy <xett03@gmail.com>
2020-03-29 17:06:25 -0400

    fixing timers

--- js/train.js
+++ js/train.js
@@ -1,6 +1,6 @@
 
 
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDE4915ncvwbOocOcdnB_elfaQLyD-Zg-c",
     authDomain: "unit-7-assignment-ceaec.firebaseapp.com",
     databaseURL: "https://unit-7-assignment-ceaec.firebaseio.com",
@@ -11,39 +11,86 @@ var firebaseConfig = {
     measurementId: "G-LH89TVS6K1"
   };
   
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 
-  //var dataRef = firebase.database();
+    
+    var database = firebase.database();
+  
 
-    // Initial Values
+    
     var TrainName = "";
     var Destination = "";
     var TrainTime = 0;
     var Frequency = 0;
 
+    var MinAway = 0;
+    var NextArrival = moment.unix(empStart).format("MM/DD/YYYY");
 
 
-$("#buttonAdd").on("click", function(event) {
+$(".buttonAddTrain").on("click", function(event) {
 
     event.preventDefault();
 
-    TrainName = $(".trainName").val().trim();
-    Destination = $(".destination").val().trim();
-    TrainTime = $(".firstTrain").val().trim();
-    Frequency = $(".frequency").val().trim();
+    NewTrainName = $(".trainName").val().trim();
+    NewDestination = $(".destination").val().trim();
+    NewTrainTime = $(".firstTrain").val().trim();
+    NewFrequency = $(".frequency").val().trim();
 
-    dataRef.ref().push({
+    database.ref().push({
 
-        TrainName: TrainName,
-        Destination: Destination,
-        TrainTime: TrainTime,
-        Frequency: Frequency,
+        TrainName: NewTrainName,
+        Destination: NewDestination,
+        TrainTime: NewTrainTime,
+        Frequency: NewFrequency,
+        //minAway: NewMinAway,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+    
 
-console.log(TrainName)
-console.log(Destination)
-console.log(TrainTime)
-console.log(Frequency)
+    alert("hello")
+    console.log(TrainName)
+    console.log(Destination)
+    console.log(TrainTime)
+    console.log(Frequency)
+    //console.log(NewMinAway)
+
+    $(".trainName").val("");
+    $(".destination").val("");
+    $(".firstTrain").val("");
+    $(".frequency").val("");
 
 });
+
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    
+    var NewTrainName = childSnapshot.val().TrainName;
+    var NewDestination = childSnapshot.val().Destination;
+    var NewTrainTime = childSnapshot.val().TrainTime;
+    var NewFrequency = childSnapshot.val().Frequency;
+    //var NewMinAway = childSnapshot.val().minAway;
+
+    var NextArrival = moment.unix(empStart).format("MM/DD/YYYY");
+  
+    
+    console.log(NewTrainName);
+    console.log(NewDestination);
+    console.log(NewTrainTime);
+    console.log(NewFrequency);
+    //console.log(NewMinAway);
+  
+    
+    
+    var newRow = $("<tr>").append(
+      $("<td>").text(NewTrainName),
+      $("<td>").text(NewDestination),
+      $("<td>").text(NewFrequency),
+      $("<td>").text(NewFrequency),
+      //$("<td>").text(NewMinAway),
+    );
+  
+    
+    $("#tableBody").append(newRow);
+  });
