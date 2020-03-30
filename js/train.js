4746e37318d2ee7d54705725d05b82c5e7269662
Yuriy <xett03@gmail.com>
2020-03-30 04:36:32 -0400

    basic+clock

--- js/train.js
+++ js/train.js
@@ -27,14 +27,13 @@ var Frequency = "";
 var clockDiv = $("<div>");
     clockDiv.attr("id", "clock");
     $("#header").append(clockDiv);
-    
+
 
 function clock() {
 
-  var currentTime = moment().format("MMM Do HH:mm:ss");
-      $("#clock").text(currentTime);
+  var currentTime1 = moment().format("MMM Do HH:mm:ss");
+      $("#clock").text(currentTime1);
       setTimeout(clock, 1000);
-
 };
     
 
@@ -65,15 +64,11 @@ $(".buttonAddTrain").on("click", function(event) {
   $(".firstTrain").val("");
   $(".frequency").val("");
 
-
 });
-
-
 
 
 database.ref().on("child_added", function(childSnapshot) {
   
-    
 
 var NewTrainName = childSnapshot.val().TrainName;
 var NewDestination = childSnapshot.val().Destination;
@@ -82,27 +77,23 @@ var NewFrequency = childSnapshot.val().Frequency;
 
 
 var firstTimeConverted = moment(NewTrainTime, "HH:mm")
-
+var currentTime = moment().format("MMM Do HH:mm:ss");
 var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 var tRemainder = diffTime % NewFrequency;
 var MinAway = NewFrequency - tRemainder;
 var NextArrival = moment().add(MinAway, "minutes");
 var NewNextArrival = moment(NextArrival).format("MMM Do, HH:mm")
-var key = childSnapshot.key;   
 
 
-
-    
+console.log(currentTime);
 console.log(NewTrainName);
 console.log(NewDestination);
 console.log(NewTrainTime);
 console.log(NewFrequency);
     
-
-
-
     
 var newRow = $("<tr>").append(
+
   $("<td>").text(NewTrainName),
   $("<td>").text(NewDestination),
   $("<td>").text(NewFrequency),
@@ -110,21 +101,9 @@ var newRow = $("<tr>").append(
   $("<td>").text(MinAway),
 );
 
-newRow.addClass("DeleteMe")
-newRow.attr("id", key);
-console.log(newRow);
 
 $("#tableBody").append(newRow);
-
-
-document.getElementById(key).onclick = function() {
-  alert("hello")
-  console.log(this)
-};
-
 
 });
 
 clock();
-
-
